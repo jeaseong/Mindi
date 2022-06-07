3ea71c3b239371dfbc9a7a957abe40dd8fa23940
12yoonj <12yoonj@gmail.com>
2022-06-07 15:34:13 +0900

    BE: feat: swagger 세팅

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,15 +1,23 @@
 import cors from "cors";
 import morgan from "morgan";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+// import yaml from 'js-yaml';
+import YAML from 'yamljs'
+import path from "path";
+
 
 import { Request, Response, NextFunction } from "express";
 
 const app: express.Application = express();
 
+const swaggerSpec = YAML.load(path.join(__dirname, './modules/swagger.yaml'))
+
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup((swaggerSpec), {explorer: true }))
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello, world!");

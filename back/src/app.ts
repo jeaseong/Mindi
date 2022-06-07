2acaf95849db467b00b4584d45149d918a56e368
12yoonj <12yoonj@gmail.com>
2022-06-07 21:00:50 +0900

    feat: 프리티어 적용

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,25 +1,25 @@
-import cors from "cors";
-import morgan from "morgan";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import yaml from 'js-yaml'
-import path from "path";
-import connectDB from "./loaders/mongoose";
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import path from 'path';
+import connectDB from './loaders/mongoose';
 
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 const app: express.Application = express();
-const swaggerSpec: any = yaml.load(path.join(__dirname, './modules/swagger.yaml'))
-connectDB()
+const swaggerSpec: any = yaml.load(path.join(__dirname, './modules/swagger.yaml'));
+connectDB();
 
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup((swaggerSpec), {explorer: true }))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello, world!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello, world!');
 });
 
 export { app };

edc3ad1306ec66857973301b5eeb56831c616ecf
12yoonj <12yoonj@gmail.com>
2022-06-08 11:17:45 +0900

    BE: chore: swagger 수정 확인 위한 커밋

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,27 +1,20 @@
 import 'reflect-metadata';
-import config from "./config";
-import loader from "./loaders";
-import swaggerUi from 'swagger-ui-express';
-import yaml from 'js-yaml';
-import path from 'path';
-import express from "express";
-import { Request, Response, NextFunction } from "express";
+import config from './config';
+import loader from './loaders';
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 async function appStart() {
   const app: express.Application = express();
 
-  const swaggerSpec: any = yaml.load(path.join(__dirname, './modules/swagger.yaml'));
-
   await loader({ expressApp: app });
-
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 
   app.listen(config.port, () => {
     console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${config.port}`);
   });
 
-  app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello, world!");
+  app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello, world!');
   });
 }
 
--- back/src/loaders/express.ts
+++ back/src/loaders/express.ts
@@ -1,16 +1,21 @@
-import cors from "cors";
-import morgan from "morgan";
-import express from "express";
-import routers from "../api";
-import errorHandler from "../api/middlewares/errorHandler";
-import config from "../config";
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+import routers from '../api';
+import errorHandler from '../api/middlewares/errorHandler';
+import config from '../config';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import path from 'path';
 
 export default ({ app }: { app: express.Application }) => {
+  const swaggerSpec: any = yaml.load(path.join(__dirname, './modules/swagger.yaml'));
+
   app.use(cors());
-  app.use(morgan("tiny"));
+  app.use(morgan('tiny'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
   app.use(config.api.prefix, routers());
 
   app.use(errorHandler);

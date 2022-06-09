3698ff0ecd7931be7050518c0903c87ea6056efb
jjssong <wkdthdrnt@naver.com>
2022-06-09 21:31:47 +0900

    [FE][]chore: 서비스 내 특정 문자열 상수화

---
+++ front/src/components/utils/constants.ts
@@ -0,0 +1,18 @@
+export const LABEL = {
+  SIGNIN: {
+    label: '로그인',
+    value: 'signIn',
+  },
+  SINGUP: {
+    label: '회원가입',
+    value: 'signUp',
+  },
+  EMAIL: {
+    label: 'email',
+    value: 'email',
+  },
+  PASSWORD: {
+    label: 'password',
+    value: 'password',
+  },
+};

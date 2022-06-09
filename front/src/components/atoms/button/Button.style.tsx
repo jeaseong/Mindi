7942270a97ab8695cc730ac03204b88b0c78f79e
jjssong <wkdthdrnt@naver.com>
2022-06-09 21:27:30 +0900

    [FE][#13]style: atomic 버튼 크기 설정

--- front/src/components/atoms/button/Button.style.tsx
+++ front/src/components/atoms/button/Button.style.tsx
@@ -3,11 +3,11 @@ import { BtnProps } from 'components/types/button';
 
 export const Btn = styled.button<BtnProps>`
   background-color: ${(props) => props.theme.colors.btnColor};
-  width: 70px;
-  height: 30px;
+  width: 150px;
+  height: 40px;
   font-size: 0.7rem;
   border: none;
-  border-radius: 10px;
+
   cursor: pointer;
   transition: all 0.1s ease-in-out;
   &:hover {

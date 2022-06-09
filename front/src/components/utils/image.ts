4327aaa468ce8ea1f554674505bc41b1ca7a9d81
jjssong <wkdthdrnt@naver.com>
2022-06-09 21:31:19 +0900

    [FE][]chore: 이미지 경로 상수 관리

---
+++ front/src/components/utils/image.ts
@@ -0,0 +1,10 @@
+export const IMAGE = {
+  AUTH_LOGO: {
+    url: `${process.env.PUBLIC_URL}/assets/auth_logo.png`,
+    alt: '서비스 로고',
+  },
+  AUTH_MIDDLE: {
+    url: `${process.env.PUBLIC_URL}/assets/auth_middle.png`,
+    alt: '중앙선',
+  },
+};

c6da4847e7a66bf5fe8fe7ad1f39f378c65be979
jjssong <wkdthdrnt@naver.com>
2022-06-10 00:57:20 +0900

    [FE][#13]style: auth molecule 스타일

---
+++ front/src/components/molecule/auth/Auth.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AuthContainer = styled.form`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;

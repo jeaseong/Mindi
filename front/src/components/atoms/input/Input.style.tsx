d9047961e1574f8f3b000a4af09958ee5201032b
jjssong <wkdthdrnt@naver.com>
2022-06-10 20:41:56 +0900

    [FE][#13]style: 간격조정

--- front/src/components/atoms/input/Input.style.tsx
+++ front/src/components/atoms/input/Input.style.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export const InputBox = styled.input`
-  width: 70%;
+  width: 100%;
   font-size: 0.8rem;
   padding: 10px;
   border: none;
   border-bottom: 1px solid ${(props) => props.theme.colors.basic};
+  background-color: inherit;
   &:focus {
     outline: none;
   }
--- front/src/components/molecule/auth/Auth.style.tsx
+++ front/src/components/molecule/auth/Auth.style.tsx
@@ -5,5 +5,11 @@ export const AuthContainer = styled.form`
   width: 100%;
   flex-direction: column;
   align-items: center;
-  gap: 30px;
+  gap: 10px;
+  margin: 0 auto;
+`;
+
+export const InputBox = styled.div`
+  width: 70%;
+  min-height: 55px;
 `;

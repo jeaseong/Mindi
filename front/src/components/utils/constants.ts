0a5ed6e6af66956e877f6f76bfe22b3a523a5129
jjssong <wkdthdrnt@naver.com>
2022-06-10 20:50:44 +0900

    [FE][#13]chore: 로그인, 회원가입 안내문

--- front/src/components/utils/constants.ts
+++ front/src/components/utils/constants.ts
@@ -20,7 +20,26 @@ export const LABEL = {
     value: 'password',
   },
   CONFIRM: {
-    label: 'confirm',
+    label: 'confirmPassword',
     value: 'confirm',
+  },
+  NOMEMBER: {
+    label: '아직 회원이 아니신가요?',
+    value: 'visitor',
+  },
+};
+
+export const SIGNIN_GUIDE = {
+  EMAIL: {
+    label: '이메일 형식이 올바르지 않습니다.',
+  },
+  PASSWORD: {
+    label: '영문 + 숫자 + 8자리 이상입니다.',
+  },
+  CONFIRM_PASSWORD: {
+    label: '비밀번호가 일치하지 않습니다.',
+  },
+  NAME: {
+    label: '두 글자 이상 적어주세요.',
   },
 };

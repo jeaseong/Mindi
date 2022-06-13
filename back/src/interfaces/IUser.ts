0e231c2c6c53738c9203b5bba12e11390b2fc181
Jaeyeon Kim <fobbb77@gmail.com>
2022-06-13 23:27:59 +0900

    [BE] chore: IUser.ts 필요없는 주석 삭제

--- back/src/interfaces/IUser.ts
+++ back/src/interfaces/IUser.ts
@@ -8,16 +8,3 @@ export interface IUser {
   createdAt: string;
   updatedAt: string;
 }
-
-// export interface IUser extends Partial<User> {}
-
-// export interface IUser {
-//   _id?: string;
-//   name?: string;
-//   email?: string;
-//   password?: string;
-//   role?: string;
-//   recentLogin?: string;
-//   createdAt?: string;
-//   updatedAt?: string;
-// }

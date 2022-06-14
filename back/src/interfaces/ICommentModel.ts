7ba730b20cf51a924790f991517b90d7e4aa8d1c
Jaeyeon Kim <fobbb77@gmail.com>
2022-06-14 21:40:21 +0900

    [BE][#28] feat: Comment model 추가

--- back/src/interfaces/ICommentModel.ts
+++ back/src/interfaces/ICommentModel.ts
@@ -1,10 +1,10 @@
 import { IComment } from "./IComment";
 
 export interface ICommentModel {
-  create: (postId: string, content: string) => Promise<Partial<IComment>>;
+  create: (post: string, content: string, author: string) => Promise<Partial<IComment>>;
   update: (filter: Object, fieldToUpdate: Object) => Promise<Partial<IComment>|null>;
   delete: (commentId: string) => Promise<void>;
   findOne: (filter: Object) => Promise<Partial<IComment>|null>;
-  findMany: (filter: Object) => Promise<Array<Partial<IComment>>|null>;
+  findMany: (filter: Object | null, query: { page: number, limit: number }) => Promise<Array<Partial<IComment>>|null>;
   exists: (filter: Object) => Promise<Boolean>;
 }
---
+++ back/src/models/comment.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import { IComment } from "../interfaces/IComment";
+import { ICommentModel } from "../interfaces/ICommentModel";
+import { Service } from "typedi";
+
+export const Comment = new mongoose.Schema({
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+}, { timestamps: true });
+
+export const CommentModel = mongoose.model<IComment>("Comment", Comment);
+
+@Service()
+export class MongoCommentModel implements ICommentModel {
+  async create(post: string, content: string, author: string) {
+    const comment = await CommentModel.create({ post, content, author });
+    return comment.toObject();
+  };
+  async update(filter: Object, fieldToUpdate: Object) {
+    return CommentModel.findOneAndUpdate(
+      filter,
+      { $set: fieldToUpdate },
+      { returnOriginal: false }
+    );
+  };
+  async delete(commentId: string) {
+    await CommentModel.deleteOne({ _id: commentId });
+  };
+  async findOne(filter: Object) {
+    return CommentModel.findOne(filter).lean();
+  };
+  async findMany(filter: Object | null, query: { page: number, limit: number }) {
+    if (filter === null) {
+      return CommentModel
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(query.limit)
+        .skip((query.page - 1) * query.limit)
+        .lean();
+    }
+    else {
+      return CommentModel
+        .find(filter)
+        .sort({ createdAt: -1 })
+        .limit(query.limit)
+        .skip((query.page - 1) * query.limit)
+        .lean();
+    }
+  };
+  async exists(filter: Object) {
+    const res = CommentModel.exists(filter).lean();
+    return !!res;
+  };
+}

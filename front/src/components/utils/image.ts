3a4752d8175288a54a82b653b0bb813592c2979b
Eunyeoul Lee <yiio08@naver.com>
2022-06-16 00:12:26 +0900

    [FE][] chore: 이미지 url 추가

--- front/src/components/utils/image.ts
+++ front/src/components/utils/image.ts
@@ -7,4 +7,9 @@ export const IMAGE = {
     url: `${process.env.PUBLIC_URL}/assets/auth_middle.png`,
     alt: '중앙선',
   },
+
+  LOGO_EFFECT_LINE: {
+    url: `${process.env.PUBLIC_URL}/assets/logo_effect_line.png`,
+    alt: '로고 집중선',
+  },
 };

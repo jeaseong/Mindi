4e59fcc03e2c1c6edaf7d9eb8073872e45ff2a25
jjssong <wkdthdrnt@naver.com>
2022-06-16 02:04:59 +0900

    [FE][#31]atom: textarea atom 개발

---
+++ front/src/components/atoms/textArea/TextArea.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TextEdit } from './TextArea.style';
+import { TextAreaProps } from 'components/types/atoms';
+
+function TextArea({ bgColor = 'green', onChange }: TextAreaProps) {
+  return <TextEdit onChange={onChange} bgColor={bgColor}></TextEdit>;
+}
+
+export default React.memo(TextArea);

a64358688e8e1a6df8afab52ac4e9b38f3e03476
12yoonj <12yoonj@gmail.com>
2022-06-16 02:04:08 +0900

    [BE][] refactor: 이미지를 네이버 플랫폼 저장소에 저장하도록 수정

--- back/src/config/index.ts
+++ back/src/config/index.ts
@@ -1,8 +1,8 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 const envFound = dotenv.config();
 if (envFound.error) {
-  throw new Error(".env 파일을 만들어주세요.️");
+  throw new Error('.env 파일을 만들어주세요.️');
 }
 
 export default {
@@ -10,11 +10,11 @@ export default {
   port: process.env.SERVER_PORT || 5000,
 
   // 데이터베이스 URI
-  dbURI: process.env.MONGODB_URI || "MONGODB_URI does not exist in .env file.",
+  dbURI: process.env.MONGODB_URI || 'MONGODB_URI does not exist in .env file.',
 
   // jwt 토큰 생성을 위한 secret key
   jwtSecretKey: process.env.JWT_SECRET_KEY,
-  jwtExpirationDate: "1d",
+  jwtExpirationDate: '1d',
 
   // 구글 인증을 위한 클라이언트 정보
   googleClientId: process.env.GOOGLE_CLIENT_ID,
@@ -22,6 +22,12 @@ export default {
 
   // api URI의 기본 경로
   api: {
-    prefix: "/api"
-  }
+    prefix: '/api',
+  },
+
+  s3AccessKey: process.env.NCLOUD_ACCESS_KEY,
+  s3SecretKey: process.env.NCLOUD_SECRET_KEY,
+  s3Endpoint: process.env.NCLOUD_ENDPOINT,
+  s3region: process.env.NCLOUD_REGION,
+  bucketName: process.env.NCLOUD_BUCKET_NAME,
 };

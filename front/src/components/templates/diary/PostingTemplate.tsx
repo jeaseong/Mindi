8ebf5a3f3ad6da627b5a289f06d2eae2efef9288
jjssong <wkdthdrnt@naver.com>
2022-06-16 02:09:43 +0900

    [FE][#31]feat: posting template, page 개발

---
+++ front/src/components/templates/diary/PostingTemplate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Posting from 'components/modules/diary-posting/Posting';
+import { Template } from './PostingTemplate.style';
+
+function PostingTemplate() {
+  return (
+    <Template>
+      <Posting />
+    </Template>
+  );
+}
+
+export default PostingTemplate;

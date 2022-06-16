072a4e169a1124da3c2396beb8cef9b4bf8826d9
12yoonj <12yoonj@gmail.com>
2022-06-16 14:55:30 +0900

    [BE][] refactor: logger 코드 수정

--- back/src/loaders/winston.ts
+++ back/src/loaders/winston.ts
@@ -1,26 +1,35 @@
-import winston from "winston";
+import winston from 'winston';
 
 const transports = [
   new winston.transports.Console({
-    level: "info",
+    level: 'info',
     format: winston.format.combine(
       winston.format.timestamp({
-        format: "YYYY-MM-DD HH:mm:ss"
+        format: 'YYYY-MM-DD HH:mm:ss',
       }),
       winston.format.cli(),
       winston.format.splat(),
       winston.format.errors({ stack: true }),
-    )
+      winston.format.printf((info) => `${info.level} | ${info.message}`),
+    ),
   }),
   new winston.transports.File({
-    filename: "errors.log",
-    level: "error"
-  })
+    filename: 'errors.log',
+    level: 'error',
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      winston.format.splat(),
+      winston.format.errors({ stack: true }),
+      winston.format.printf((info) => `${info.timestamp} | ${info.message}`),
+    ),
+  }),
 ];
 
 const logger = winston.createLogger({
-  level: "info",
-  transports: transports
+  level: 'info',
+  transports: transports,
 });
 
 export default logger;

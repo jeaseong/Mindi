4ad0a9cc228e1c099c6262845cea6032ac0548bf
Eunyeoul Lee <yiio08@naver.com>
2022-06-16 15:22:49 +0900

    [FE][] fix: useLocation state 오류 해결

--- front/src/components/modules/EITest/EITestResult.tsx
+++ front/src/components/modules/EITest/EITestResult.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'components/atoms/image/Image';
 import { useLocation } from 'react-router-dom';
 import { IMAGE } from 'components/utils/image';
+import Button from 'components/atoms/button/Button';
 import {
   EIResultTemplate,
   ImageWrapper,
@@ -11,8 +12,21 @@ import {
   DescriptionWrapper,
   Description,
 } from './EITestResult.style';
+import { StyledButtonDiv } from './EITest.style';
+import { CustomizedLocate } from 'components/types/atoms';
+
 function EITestResult() {
+  interface CustomizedState {
+    [key: string]: string | undefined;
+  }
+
   const location = useLocation();
+  const state = location.state as CustomizedState;
+
+  React.useEffect(() => {
+    // 마운트 될 때 location 정보 출력
+    console.log(state.selections);
+  }, []);
 
   return (
     <EIResultTemplate>
@@ -33,6 +47,9 @@ function EITestResult() {
           <br /> 대나무숲에 감성 지수를 높힐 수 있는 노하우를 공유해주세요!
         </Description>
       </DescriptionWrapper>
+      <StyledButtonDiv>
+        <Button>가입하기</Button>
+      </StyledButtonDiv>
     </EIResultTemplate>
   );
 }

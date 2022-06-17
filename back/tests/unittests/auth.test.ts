335e6a37e90d5a49c3520b8f7f0edbc499065816
Jaeyeon Kim <fobbb77@gmail.com>
2022-06-18 00:16:18 +0900

    [BE][#39] test: user unit test 코드 추가

---
+++ back/tests/unittests/auth.test.ts
@@ -0,0 +1,64 @@
+import { IUserModel } from '../../src/interfaces/IUserModel';
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
+import AuthService from "../../src/services/auth";
+
+const mockObjectId = faker.database.mongodbObjectId();
+const dayString = dayjs().toISOString();
+const mockEmail = faker.internet.email();
+const mockName = faker.name.findName();
+const role = "user";
+const mockPassword = faker.internet.password();
+
+const userObject = {
+  _id: mockObjectId,
+  name: mockName,
+  email: mockEmail,
+  password: mockPassword,
+  role: role,
+  recentLogin: dayString,
+  createdAt: dayString,
+  updatedAt: dayString
+};
+
+export class TestUserModel implements IUserModel {
+  async create(email: string, name: string, password: string) {
+    return userObject;
+  }
+
+  async update(filter: Object, fieldToUpdate: Object) {
+    return userObject;
+  }
+
+  async delete(userId: string) {
+    return;
+  }
+
+  async findOne(filter: Object) {
+    return userObject;
+  }
+
+  async findMany(filter: Object) {
+    return [
+      userObject,
+      userObject,
+    ];
+  }
+
+  async exists(filter: Object) {
+    return true;
+  }
+}
+
+describe("Auth Service Test", () => {
+  const authService = new AuthService(new TestUserModel);
+
+  test("should return UserInfo.", async () => {
+    expect(await authService.localSignUp(mockEmail, mockName, mockPassword)).toEqual(userObject);
+  });
+  test("should return UserInfo and token with its expiration date", async () => {
+    expect(await authService.localSignIn(mockEmail, mockPassword)).toMatchObject(userObject);
+    expect(await authService.localSignIn(mockEmail, mockPassword)).toHaveProperty("token");
+    expect(await authService.localSignIn(mockEmail, mockPassword)).toHaveProperty("expiresIn");
+  });
+});

09f2d4a417ab2deed96c9b44500982ae7c0e43a9
Eunyeoul Lee <yiio08@naver.com>
2022-06-18 00:45:59 +0900

    [FE][] refactor: framer motion 리팩토링

--- front/src/components/modules/main/Main1.tsx
+++ front/src/components/modules/main/Main1.tsx
@@ -7,24 +7,55 @@ import { useNavigate } from 'react-router-dom';
 
 function Main1() {
   const navigate = useNavigate();
+
+  const templateVariants = {
+    hidden: {
+      y: 50,
+      rotateZ: 180,
+      opacity: 0.5,
+      scale: 0.2,
+    },
+    visible: {
+      y: 0,
+      rotateZ: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.6,
+        type: 'spring',
+        when: 'beforeChildren',
+      },
+    },
+  };
+
+  const childVariants = {
+    normal: { scale: 0 },
+    point: {
+      scale: 1,
+      transition: {
+        delay: 0.3,
+        type: 'spring',
+        stiffness: 400,
+      },
+    },
+    hover: { scale: 1.2, color: 'red' },
+  };
+
   return (
     <MainTemplate
-      initial={{ y: 50, rotateZ: 180, opacity: 0.5, scale: 0.2 }}
-      animate={{ y: 0, rotateZ: 0, opacity: 1, scale: 1 }}
-      transition={{ duration: 0.6, type: 'spring' }}
+      variants={templateVariants}
+      initial='hidden'
+      animate='visible'
     >
       <Image width='30%' src={IMAGE.NEW_LOGO.url} alt={IMAGE.NEW_LOGO.alt} />
       <MainButton
         onClick={() => {
           navigate('/main/2');
         }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ delay: 0.3, type: 'spring', stiffness: 400 }}
-        whileHover={{
-          scale: 1.2,
-          color: 'red',
-        }}
+        variants={childVariants}
+        initial='normal'
+        animate='point'
+        whileHover='hover'
       >
         CLICK!
       </MainButton>

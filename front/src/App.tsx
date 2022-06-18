939ea5761f2503b2bc62aa2031100eec5d86247e
jjssong <wkdthdrnt@naver.com>
2022-06-19 00:08:47 +0900

    [FE][#40] 일기 페이지 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from 'pages/User/SignUp';
 import Posting from 'pages/Diary/Posting';
 import Header from 'components/modules/header/Header';
 import EITest from 'pages/EITest/EITest';
+import Diary from 'pages/Diary/Diary';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EITestResult from 'components/modules/EITest/EITestResult';
 import Main from 'components/modules/main/Main';
@@ -14,9 +15,11 @@ const App: React.FC = () => {
   return (
     <div className='App'>
       <Router>
+        <Header />
         <Routes>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/diary' element={<Diary />} />
           <Route path='/diary-posting' element={<Posting />} />
           <Route path='/ei-test' element={<EITest />} />
           <Route path='/ei-test/result' element={<EITestResult />} />

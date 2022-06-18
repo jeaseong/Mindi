a5c6e190f122bb42589fa3af015b0cacfb82c0eb
12yoonj <12yoonj@gmail.com>
2022-06-18 21:42:57 +0900

    [BE][] feat: signal event에 따른 서버 및 디비 연결 종료 코드 추가

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,25 +1,35 @@
-import 'reflect-metadata';
-import config from './config';
-import loader from './loaders';
-import express from 'express';
-import { Request, Response, NextFunction } from 'express';
-import logger from './loaders/winston';
+import "reflect-metadata";
+import config from "./config";
+import loader from "./loaders";
+import express from "express";
+import { Request, Response, NextFunction } from "express";
+import logger from "./loaders/winston";
+import mongoose from "mongoose";
 
 async function appStart() {
   const app: express.Application = express();
 
   await loader({ expressApp: app });
 
-  app.listen(config.port, () => {
+  const server = app.listen(config.port, () => {
     logger.info(`
             Mindi API Server
             is running on: http://localhost:${config.port}
             `);
   });
 
-  app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello, world!');
+  app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello, world!");
   });
+
+  async function handle() {
+    await mongoose.connection.close();
+    logger.info("MongoDB: Discnnected");
+    server.close();
+    logger.info("Mindi API Server: Shutdown");
+  }
+
+  process.on("SIGINT", handle);
 }
 
 export = appStart();

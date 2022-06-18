e692edc8b1559da1074efc1441ce3483c24aa7ac
Eunyeoul Lee <yiio08@naver.com>
2022-06-18 19:19:07 +0900

    [FE][] design: result template 디자인

--- front/src/components/templates/diary/ResultTemplate.style.tsx
+++ front/src/components/templates/diary/ResultTemplate.style.tsx
@@ -1,1 +1,7 @@
 import styled from 'styled-components';
+
+export const StyledTemplate = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+`;
--- front/src/components/templates/diary/ResultTemplate.tsx
+++ front/src/components/templates/diary/ResultTemplate.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import Result from 'components/modules/diary-result/Result';
-
+import { StyledTemplate } from './ResultTemplate.style';
+import {
+  PageTitle,
+  Lines,
+  LineTop,
+  LineBottom,
+} from '../../atoms/pageTitle/pageTitle.style';
 function ResultTemplate() {
-  return <></>;
+  return (
+    <StyledTemplate>
+      <PageTitle>
+        Today&apos;s <br />
+        Emotion
+      </PageTitle>
+      <Lines>
+        <LineTop />
+        <LineBottom />
+      </Lines>
+      <Result />
+    </StyledTemplate>
+  );
 }
 
 export default ResultTemplate;

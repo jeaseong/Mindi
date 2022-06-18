6afcc1ee3f60addc52323103a6f2d42ed0368ef3
jjssong <wkdthdrnt@naver.com>
2022-06-18 15:09:09 +0900

    [FE][]chore: 폴더명 변경

--- front/src/components/templates/diary/PostingTemplate.tsx
+++ front/src/components/templates/diary/PostingTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Posting from 'components/modules/diary-posting/Posting';
+import Posting from 'components/modules/diary/posting/Posting';
 import { Template } from './PostingTemplate.style';
 
 function PostingTemplate() {

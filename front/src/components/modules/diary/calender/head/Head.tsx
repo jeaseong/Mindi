8e65d7a804806d5b3177789aedc73c4bde22315f
jjssong <wkdthdrnt@naver.com>
2022-06-22 02:49:23 +0900

    [FE][#41]style: 캘린더 head 구현

--- front/src/components/modules/diary/calender/head/Head.tsx
+++ front/src/components/modules/diary/calender/head/Head.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import { CalenderProps } from 'types/atoms';
+import Span from 'components/atoms/span/Span';
+import { CalenderHeader, Nav, NavBtn, Days, Day } from './Head.style';
+import { DATE } from 'utils/constants';
+
 function Head({ year, month, onChangeMonth }: CalenderProps) {
-  return <CalenderHeader></CalenderHeader>;
+  return (
+    <CalenderHeader>
+      <Nav>
+        <NavBtn>&lt;</NavBtn>
+        <Span>
+          {year}. {DATE.MONTH[month]}
+        </Span>
+        <NavBtn>&gt;</NavBtn>
+      </Nav>
+      <Days>
+        {DATE.DAY.en.map((d) => (
+          <Day key={d}>{d}</Day>
+        ))}
+      </Days>
+    </CalenderHeader>
+  );
 }
 
 export default React.memo(Head);

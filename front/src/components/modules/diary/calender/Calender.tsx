60a0c6bfa924d76beacd0ec8e5596601bf450920
jjssong <wkdthdrnt@naver.com>
2022-06-23 03:45:37 +0900

    [FE][#41] calender 년, 월에 따라 날짜 생성 함수

--- front/src/components/modules/diary/calender/Calender.tsx
+++ front/src/components/modules/diary/calender/Calender.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Head from './head/Head';
 import Body from './body/Body';
+import { nullCheck } from 'utils/nullCheck';
+import { CalenderBodyProps, CalenderHeadProps } from 'types/atoms';
 import { Container } from './Calender.style';
 function Calender() {
   const DATE: Date = new Date();
@@ -8,7 +10,8 @@ function Calender() {
   const MONTH: number = DATE.getMonth();
   const [month, setMonth] = useState(MONTH);
   const [year, setYear] = useState(YEAR);
-  const [totalDate, setTotalDate] = useState([]);
+  const [today, setToday] = useState(DATE.getDate());
+  const [totalDate, setTotalDate] = useState<string[][] | undefined>();
 
   const onChangeMonth = (m: number) => {
     if (month === 11 && m > 0) {
@@ -22,15 +25,54 @@ function Calender() {
     }
     setMonth((cur) => cur + m);
   };
-  const HeadProps = {
+
+  useEffect(() => {
+    // 이번 달의 1일이 무슨 요일
+    const firstDay = new Date(year, month, 1).getDay();
+    //이번 달 길이
+    const nowDayLength = new Date(year, month + 1, 0).getDate();
+    // 이번 달은 몇 주
+    const weekLength = Math.ceil((nowDayLength - (6 - firstDay + 1)) / 7) + 1;
+
+    let cnt = 1;
+    const calenderDays = [];
+    for (let i = 0; i < weekLength; i++) {
+      const days = [];
+      for (let j = 0; j < 7; j++) {
+        if (cnt > nowDayLength) {
+          days.push('');
+        } else if (firstDay > j && i === 0) {
+          days.push('');
+        } else {
+          days.push(`${cnt}`);
+          cnt++;
+        }
+      }
+      calenderDays.push(days);
+    }
+    setTotalDate(calenderDays);
+  }, [month]);
+
+  const bodyProps: CalenderBodyProps = {
+    totalDate: nullCheck(totalDate),
     year,
-    month: month,
-    onChangeMonth,
+    month,
+    today,
   };
+
+  const headProps: CalenderHeadProps = useMemo(
+    () => ({
+      year,
+      month,
+      onChangeMonth,
+    }),
+    [year, month],
+  );
+
   return (
     <Container>
-      <Head {...HeadProps}></Head>
-      <Body></Body>
+      <Head {...headProps}></Head>
+      <Body {...bodyProps}></Body>
     </Container>
   );
 }

a93ec01176234d9a04bfcdb9609f719cf14d4877
jjssong <wkdthdrnt@naver.com>
2022-06-23 03:44:05 +0900

    [FE][]style: 기본 폰트 사이즈 키움

--- front/src/components/modules/diary/diaryList/DiaryCard.style.tsx
+++ front/src/components/modules/diary/diaryList/DiaryCard.style.tsx
@@ -22,7 +22,6 @@ export const DiaryPost = styled.article`
 export const Date = styled.h3`
   display: inline-block;
   padding: 4px;
-  font-size: 0.8rem;
   background-color: ${(props) => props.theme.colors.basicWhite};
   font-weight: normal;
   margin-bottom: 8px;

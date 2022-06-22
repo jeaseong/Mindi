1059fb178a71ec6c505c2909e3185e2d693a28eb
jjssong <wkdthdrnt@naver.com>
2022-06-22 12:25:28 +0900

    [FE][]chore: 오타 수정

--- front/src/hooks/diaryQuery.tsx
+++ front/src/hooks/diaryQuery.tsx
@@ -3,7 +3,7 @@ import { getDiaryList } from 'api/api';
 
 export const useGetDiaryList = async (date: string) => {
   return useQuery(['diaryList', date], async () => {
-    const data = await getDiaryList(date);
+    const data = await getDiaryList(0, 0, 0);
     return data;
   });
 };

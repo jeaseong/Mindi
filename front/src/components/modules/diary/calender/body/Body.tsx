62764aa5123b0af78812cde9982c9d833fca7b1e
jjssong <wkdthdrnt@naver.com>
2022-06-23 03:46:44 +0900

    [FE][#41]feat: calender body 레이아웃 생성

--- front/src/components/modules/diary/calender/body/Body.tsx
+++ front/src/components/modules/diary/calender/body/Body.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-function Body() {
-  return <div></div>;
+import { CalenderBodyProps } from 'types/atoms';
+import { Container, Days, Day, Span } from './Body.style';
+
+function Body({ totalDate, year, month, today }: CalenderBodyProps) {
+  return (
+    <Container>
+      {totalDate?.map((days, index) => {
+        const dataKey = `${year}-${month}-${index}`;
+        return (
+          <Days key={dataKey}>
+            {days?.map((day, i) => {
+              const dataKey = `${year}-${month}-${i}`;
+              return (
+                <Day isNull={day.length === 0 ? true : false} key={dataKey}>
+                  <Span>{day}</Span>
+                </Day>
+              );
+            })}
+          </Days>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default React.memo(Body);

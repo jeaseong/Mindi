432922c5869542e68bc5b30df34a2cc167f1e2da
Eunyeoul Lee <yiio08@naver.com>
2022-06-24 01:30:12 +0900

    [FE][] api.ts 수정

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -2,7 +2,6 @@ import {
   customAxios,
   customAxiosFileUpload,
   customAxiosForAi,
-  customAxiosForBamboo,
 } from 'api/costomAxios';
 import { SignUpInfo, SignInInfo, diary } from 'types/apiType';
 
@@ -23,21 +22,6 @@ export const getCurUser = async () => {
   return data.result;
 };
 
-export const postBambooPosting = async (bambooData: any) => {
-  const apiUrl = `api/posts`;
-  await customAxiosForBamboo.post(apiUrl, bambooData);
-};
-
-export const postDiaryPosting = async (diaryData: any) => {
-  const apiUrl = `api/diaries`;
-  await customAxiosFileUpload.post(apiUrl, diaryData);
-};
-
-export const postAnalysis = async (diary: diary) => {
-  const apiUrl = `diaries/sentiment`;
-  const { data } = await customAxiosForAi.post(apiUrl, diary);
-  return data.result;
-};
 export const getDiaryList = async (
   year: number,
   month: number,
@@ -46,4 +30,30 @@ export const getDiaryList = async (
   const apiUrl = `/diaries?year=${year}&month=${month}&day=${day}`;
   const { data } = await customAxios.get(apiUrl);
   return data.result;
+};
+
+export const postAnalysis = async (diary: diary) => {
+  const apiUrl = `diaries/sentiment`;
+  const { data } = await customAxiosForAi.post(apiUrl, diary);
+  return data.result;
+};
+export const postDiaryPosting = async (diaryData: any) => {
+  const apiUrl = `api/diaries`;
+  await customAxiosFileUpload.post(apiUrl, diaryData);
+};
+
+export const postBambooPosting = async (bambooData: any) => {
+  const apiUrl = `api/posts`;
+  await customAxios.post(apiUrl, bambooData);
+};
+
+export const getBambooList = async () => {
+  const apiUrl = `api/users/posts`;
+  const { data } = await customAxios.get(apiUrl);
+  return data.result;
+};
+
+export const postBambooCommentPosting = async (bambooData: any) => {
+  const apiUrl = `api/posts/comments`;
+  await customAxios.post(apiUrl, bambooData);
 };

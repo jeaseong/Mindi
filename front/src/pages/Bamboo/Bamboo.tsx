3b51eb2f441d0fc418f4a7e38e6039a93a6b5f8e
Eunyeoul Lee <yiio08@naver.com>
2022-06-24 00:59:42 +0900

    [FE][] Bomboo page 연결

---
+++ front/src/pages/Bamboo/Bamboo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BambooTemplate from 'components/templates/bamboo/bambooTemplate';
+
+const Bamboo = () => {
+  return <BambooTemplate />;
+};
+
+export default Bamboo;

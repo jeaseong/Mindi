59b2fa223d23ffd7e249ed46f4fca939b6c2be05
jjssong <wkdthdrnt@naver.com>
2022-06-26 04:47:50 +0900

    [FE][#51] put react-query 요청 함수 구현, 코드가 거의똑같아서 수정 필요

--- front/src/hooks/diaryQuery.tsx
+++ front/src/hooks/diaryQuery.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { getDiaryList, postDiaryPosting } from 'api/api';
+import { getDiaryList, postDiaryPosting, putDiaryPosting } from 'api/api';
 
 export const useGetDiaryList = (year: string, month: string, day: string) => {
   const { isFetching, isLoading, error, data } = useQuery(
@@ -30,6 +30,32 @@ export const usePostDiary = (
           refetchInactive: true,
         });
         openSnackBar(true, '일기를 분석을 성공했습니다.');
+        navigate('/result', {
+          state: {
+            date,
+          },
+        });
+      },
+      onError: () => {
+        openSnackBar(false, '일기 날짜와 내용을 확인해주세요.');
+      },
+    },
+  );
+};
+export const useEditDiary = (
+  openSnackBar: (sucessAlert: boolean, msg: string) => void,
+  date: string,
+) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation(
+    async (diaryData: any) => await putDiaryPosting(diaryData),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['diary', `${date.slice(0, 7)}-00`], {
+          refetchInactive: true,
+        });
+        openSnackBar(true, '일기를 수정했습니다. 새로 일기를 분석했습니다.');
         navigate('/result', {
           state: {
             date,

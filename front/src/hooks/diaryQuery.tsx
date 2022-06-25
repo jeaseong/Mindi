8a338636f33c8bf074ff90de66c327816d655039
jjssong <wkdthdrnt@naver.com>
2022-06-25 18:59:01 +0900

    [FE][]feat: postDiary mutate 함수 작성

--- front/src/hooks/diaryQuery.tsx
+++ front/src/hooks/diaryQuery.tsx
@@ -1,13 +1,37 @@
-import { useQuery } from 'react-query';
-import { getDiaryList } from 'api/api';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { getDiaryList, postDiaryPosting } from 'api/api';
 
 export const useGetDiaryList = (year: string, month: string, day: string) => {
-  const { isFetching, error, data } = useQuery(
+  const { isFetching, isLoading, error, data } = useQuery(
     ['diary', `${year}-${month}-${day}`],
     async () => await getDiaryList(year, month, day),
     {
       staleTime: Infinity,
+      onError: () => {
+        return '데이터가 없다.';
+      },
     },
   );
-  return { diary: data, isFetching, error };
+  return { diary: data, isFetching, isLoading, error };
+};
+
+export const usePostDiary = (
+  openSnackBar: (sucessAlert: boolean, msg: string) => void,
+  date: string,
+) => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    async (diaryData: any) => await postDiaryPosting(diaryData),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['diary', `${date.slice(0, 7)}-00`], {
+          refetchInactive: true,
+        });
+        openSnackBar(true, '일기를 분석 중입니다.');
+      },
+      onError: () => {
+        openSnackBar(false, '오늘의 일기를 작성해주세요');
+      },
+    },
+  );
 };

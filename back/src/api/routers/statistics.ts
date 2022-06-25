c1606e504aeedb93ad37b0d309cc492806da5ea5
12yoonj <12yoonj@gmail.com>
2022-06-25 18:02:42 +0900

    [BE] refactor: ml 통신 로직을 서비스 계층으로 이동

--- back/src/api/routers/statistics.ts
+++ back/src/api/routers/statistics.ts
@@ -1,14 +1,14 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { Container } from "typedi";
 import { loginRequired, validationErrorChecker } from "../middlewares";
-import { IDiary, ISentiment, IStat, IResponse } from "../../interfaces";
+import { IStat, IResponse } from "../../interfaces";
 import { statValidator } from "../middlewares/express-validator";
-import { postKeywordAnalysis } from "../../utils";
-import { StatService } from "../../services";
+import { StatService, MLService } from "../../services";
 
 export default (app: Router) => {
   const statRouter = Router();
   const statService = Container.get(StatService);
+  const mlService = Container.get(MLService);
 
   app.use("/statistics", statRouter);
 
@@ -20,56 +20,18 @@ export default (app: Router) => {
     async (req: Request, res: Response, next: NextFunction) => {
       try {
         const userId = req.user!._id;
-
         const { year, month } = req.query;
         const date: string = `${year}-${month}`;
 
-        const diaries: IDiary[] = await statService.findDiaryList(userId, date);
+        const { docList, myKeyword } = await mlService.postKeywordAnalysis(userId, date); // 다이어리 모델의 다큐먼트 리스트와 키워드 분석 결과를 반환
 
-        // 키워드 통계
-        let diaryList: Array<string> = [];
-        diaries.forEach((doc) => {
-          diaryList.push(doc.diary);
-        });
-        const myKeyword = await postKeywordAnalysis({ diary: diaryList });
-
-        // 감정 통계
-        let myEmotion: ISentiment = {
-          fear: 0,
-          surprised: 0,
-          anger: 0,
-          sadness: 0,
-          happiness: 0,
-          aversion: 0,
-        };
-        diaries.forEach((doc) => {
-          doc.sentiment.fear > 0 ? myEmotion.fear++ : myEmotion.fear;
-          doc.sentiment.surprised > 0 ? myEmotion.surprised++ : myEmotion.surprised;
-          doc.sentiment.anger > 0 ? myEmotion.anger++ : myEmotion.anger;
-          doc.sentiment.sadness > 0 ? myEmotion.sadness++ : myEmotion.sadness;
-          doc.sentiment.happiness > 0 ? myEmotion.happiness++ : myEmotion.happiness;
-          doc.sentiment.aversion > 0 ? myEmotion.aversion++ : myEmotion.aversion;
-        });
-
-        // 가장 자주 관찰된 감정 추리기
-        let mostEmotion = Object.entries(myEmotion).reduce((a, b) => (a[1] > b[1] ? a : b));
-
-        // 높은 값이 여러 개일 때, 랜덤하게 하나 지정
-        const found = Object.entries(myEmotion).filter((emotion) => emotion[1] === mostEmotion[1]);
-        if (found.length > 1) {
-          const randomIdx = Math.floor(Math.random() * found.length);
-          mostEmotion = found[randomIdx];
-        }
-        const reminder: IDiary[] = await statService.findMostEmotionalDiary(userId, mostEmotion[0]);
-
-        const newResult: Partial<IStat> = {
+        const newStat: Partial<IStat> = {
           userId,
           monthly: date,
           keywords: myKeyword,
-          emotions: myEmotion,
-          reminder,
         };
-        const createdResult: IStat = await statService.create(userId, newResult);
+
+        const createdResult: IStat = await statService.create(newStat, docList);
 
         const response: IResponse<IStat> = {
           success: true,

61f7a72805cbedf1955e1e307c8c0ff153c72645
Eunyeoul Lee <yiio08@naver.com>
2022-06-25 22:35:42 +0900

    [FE][] feat: 오늘의 날짜

--- front/src/components/modules/bamboo/posting/bambooPosting.tsx
+++ front/src/components/modules/bamboo/posting/bambooPosting.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function BambooPosting() {
-  return <></>;
+  const today = new Date();
+
+  const month = ('0' + (today.getMonth() + 1)).slice(-2);
+  const year = today.getFullYear();
+  const day = ('0' + today.getDate()).slice(-2);
+
+  const dateString = year + '. ' + month + '. ' + day + '. ';
+
+  return (
+    <>
+      <h1>{dateString}</h1>
+    </>
+  );
 }
 
 export default BambooPosting;

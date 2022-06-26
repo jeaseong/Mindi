78add059ecba16af98358a022fc88e9d62f6b7ec
Eunyeoul Lee <yiio08@naver.com>
2022-06-26 16:42:37 +0900

    [FE][] style: 글쓰기 버튼 추가

--- front/src/components/templates/bamboo/bambooTemplate.style.tsx
+++ front/src/components/templates/bamboo/bambooTemplate.style.tsx
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-export const Wrap = styled.div`
+export const TitleWrap = styled.section`
+  margin: 0 auto;
+  text-align: left;
+`;
+
+export const CardWrap = styled.section`
   max-width: 60%;
   width: 100%;
   margin: 0 auto;
+`;
+
+export const ButtonWrap = styled.div`
+  max-width: 80%;
+  text-align: right;
+`;
+
+export const WriteButton = styled.button`
+  background: none;
+  border: none;
+  margin-bottom: 1rem;
+
+  cursor: pointer;
 `;
--- front/src/components/templates/bamboo/bambooTemplate.tsx
+++ front/src/components/templates/bamboo/bambooTemplate.tsx
@@ -6,23 +6,34 @@ import {
   LineTop,
   LineBottom,
 } from '../../atoms/pageTitle/pageTitle.style';
-import { Wrap } from './bambooTemplate.style';
+import {
+  CardWrap,
+  WriteButton,
+  ButtonWrap,
+  TitleWrap,
+} from './bambooTemplate.style';
 
 function BambooTemplate() {
   return (
     <>
-      <PageTitle>
-        Bamboo
-        <br />
-        Grove
-      </PageTitle>
+      <TitleWrap>
+        <PageTitle>
+          Bamboo
+          <br />
+          Grove
+        </PageTitle>
+      </TitleWrap>
+      <ButtonWrap>
+        <WriteButton>글쓰기</WriteButton>
+      </ButtonWrap>
       <Lines>
         <LineTop />
         <LineBottom />
       </Lines>
-      <Wrap>
+
+      <CardWrap>
         <BambooCard />
-      </Wrap>
+      </CardWrap>
     </>
   );
 }

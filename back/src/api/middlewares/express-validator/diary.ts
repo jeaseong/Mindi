04ca04b8e5ec174cac4deb3081cccc31e4fa8048
12yoonj <12yoonj@gmail.com>
2022-06-26 15:59:39 +0900

    [BE] fix: 과거 날짜만 일기 작성가능하도록 수정

--- back/src/api/middlewares/express-validator/diary.ts
+++ back/src/api/middlewares/express-validator/diary.ts
@@ -1,4 +1,6 @@
 import { body, check } from "express-validator";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 
 export default {
   diaryBody: [
@@ -19,7 +21,15 @@ export default {
       .withMessage("존재하지 않는 날짜입니다.")
       .bail()
       .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("날짜 형식이 올바르지 않습니다."),
+      .withMessage("날짜 형식이 올바르지 않습니다.")
+      .bail()
+      .custom((value) => {
+        const diaryDate = dayjs(value).locale("ko");
+        const today = dayjs().locale("ko");
+        const checkDate = today.diff(diaryDate, "day");
+        return checkDate > 0;
+      })
+      .withMessage("과거 날짜의 일기만 작성할 수 있습니다."),
   ],
   getYear: [check("year").notEmpty().withMessage("연도 정보는 필수입니다.")],
 };

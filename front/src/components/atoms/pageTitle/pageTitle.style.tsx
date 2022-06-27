247f4b543e758586995ca713aa1d6e90ea5259c6
jjssong <wkdthdrnt@naver.com>
2022-06-27 14:28:33 +0900

    [FE][]style: title 스타일

--- front/src/components/atoms/pageTitle/pageTitle.style.tsx
+++ front/src/components/atoms/pageTitle/pageTitle.style.tsx
@@ -4,29 +4,28 @@ export const LineTop = styled.hr`
   border: none;
   height: 2px;
   background-color: black;
-  width: 60%;
-  border-radius: 2em;
+  width: 100%;
+  border-radius: 10px;
+  margin-bottom: 10px;
 `;
 
 export const LineBottom = styled.hr`
-  margin-top: 1em;
   border: none;
   height: 1px;
   background-color: black;
-  width: 60%;
-  border-radius: 2em;
+  width: 100%;
+  border-radius: 10px;
 `;
 
 export const Lines = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 6em;
+  margin-bottom: 20px;
 `;
 
 export const PageTitle = styled.h1`
-  margin: 0.7em 0 0.1em 0;
-  padding: 0em 3.7em;
-  font-size: 96px;
+  margin: 0;
+  font-size: 70px;
   font-family: 'Courier New', Courier, monospace;
 `;

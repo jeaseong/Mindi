770632140be8b7bffd05cae02082e0a566591cdb
12yoonj <12yoonj@gmail.com>
2022-06-28 22:57:29 +0900

    [BE] feat: 비밀번호 찾기 기능 구현

--- back/src/api/middlewares/express-validator/user.ts
+++ back/src/api/middlewares/express-validator/user.ts
@@ -2,15 +2,18 @@ import { body } from "express-validator";
 
 export default {
   userUpdateBody: [
-    body("name")
-      .exists({ checkNull: true })
-      .isString()
-      .trim(),
-    body("password")
-      .exists({ checkNull: true })
-      .isString(),
-    body("description")
-      .exists({ checkNull: true })
-      .isString(),
+    body("name").exists({ checkNull: true }).isString().trim(),
+    body("password").exists({ checkNull: true }).isString(),
+    body("description").exists({ checkNull: true }).isString(),
+  ],
+  checkEmail: [
+    body("email")
+      .notEmpty()
+      .withMessage("이메일 정보는 필수입니다.")
+      .bail()
+      .isEmail()
+      .withMessage("이메일 형식이 올바르지 않습니다")
+      .bail()
+      .normalizeEmail(),
   ],
 };
--- back/src/services/user.ts
+++ back/src/services/user.ts
@@ -1,24 +1,23 @@
-import {Inject, Service} from "typedi";
-import { StatusError } from "../utils/error";
+import { Inject, Service } from "typedi";
+import { StatusError, setResetPasswordMail } from "../utils";
 import { MongoUserModel } from "../models/user";
 import winston from "winston";
+import transporter from "../loaders/email";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 @Service()
 export default class UserService {
   constructor(
     private userModel: MongoUserModel,
-    @Inject("logger") private logger: winston.Logger
-  ) {
-  }
+    @Inject("logger") private logger: winston.Logger,
+  ) {}
 
   public async getUserInfo(userId: string) {
     const userExists = await this.userModel.exists({ _id: userId });
 
     if (!userExists) {
-      throw new StatusError(
-        400,
-        "사용자가 존재하지 않습니다."
-      );
+      throw new StatusError(400, "사용자가 존재하지 않습니다.");
     }
 
     return this.userModel.findOne({ _id: userId });
@@ -28,10 +27,7 @@ export default class UserService {
     const userExists = await this.userModel.exists({ _id: userId });
 
     if (!userExists) {
-      throw new StatusError(
-        400,
-        "사용자가 존재하지 않습니다."
-      );
+      throw new StatusError(400, "사용자가 존재하지 않습니다.");
     }
 
     return this.userModel.update({ _id: userId }, fieldToUpdate);
@@ -41,12 +37,30 @@ export default class UserService {
     const userExists = await this.userModel.exists({ _id: userId });
 
     if (!userExists) {
-      throw new StatusError(
-        400,
-        "사용자가 존재하지 않습니다."
-      );
+      throw new StatusError(400, "사용자가 존재하지 않습니다.");
     }
 
     return this.userModel.delete(userId);
+  }
+
+  public async resetPassword(email: string) {
+    try {
+      const tempPassword = crypto.randomBytes(10).toString("base64");
+      const hashedPassword: string = await bcrypt.hash(tempPassword, 10);
+      const userInfo = await this.userModel.update({ email }, { password: hashedPassword });
+      return { userInfo, tempPassword };
+    } catch (error) {
+      throw new StatusError(400, "비밀번호 초기화에 실패하였습니다.");
+    }
+  }
+
+  public async sendMail(email: string, name: string, tempPassword: string) {
+    try {
+      const mailOptions = await setResetPasswordMail(email, name, tempPassword);
+      await transporter.sendMail(mailOptions);
+      await transporter.close();
+    } catch (error) {
+      throw new StatusError(400, "이메일 전송에 실패하였습니다.");
+    }
   }
 }

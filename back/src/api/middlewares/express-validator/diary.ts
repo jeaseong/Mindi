f05d59d6b50610bce22e6863a35e6b590310325e
12yoonj <12yoonj@gmail.com>
2022-06-28 18:54:16 +0900

    [BE] fix: 유효성 검사 수정

--- back/src/api/middlewares/express-validator/diary.ts
+++ back/src/api/middlewares/express-validator/diary.ts
@@ -5,7 +5,7 @@ import "dayjs/locale/ko";
 export default {
   diaryBody: [
     body("diary")
-      .isLength({ min: 3 })
+      .isLength({ min: 3 }) //TODO: 몇 자 이상 작성하라고 하지?
       .withMessage("오늘의 일기를 작성해주세요.")
       .bail()
       .isString()
@@ -16,20 +16,17 @@ export default {
       .bail()
       .isString()
       .bail(),
-    check("diaryDate")
+    body("diaryDate")
       .isISO8601()
       .withMessage("존재하지 않는 날짜입니다.")
       .bail()
-      .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("날짜 형식이 올바르지 않습니다.")
-      .bail()
       .custom((value) => {
-        const diaryDate = dayjs(value).locale("ko");
+        // const diaryDate = dayjs(value);
         const today = dayjs().locale("ko");
-        const checkDate = today.diff(diaryDate, "day");
-        return checkDate > 0;
+        const checkDate = today.diff(value, "day");
+        return checkDate >= 0;
       })
-      .withMessage("과거 날짜의 일기만 작성할 수 있습니다."),
+      .withMessage("오늘 또는 과거 날짜의 일기만 작성할 수 있습니다."),
   ],
   getYear: [check("year").notEmpty().withMessage("연도 정보는 필수입니다.")],
 };

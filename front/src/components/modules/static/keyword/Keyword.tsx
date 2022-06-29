2221e28217dbb7ec7dabcfc59ed7cbe08fced0a5
jjssong <wkdthdrnt@naver.com>
2022-06-30 05:09:41 +0900

    [FE][#55]feat: keyword

--- front/src/components/modules/static/keyword/Keyword.tsx
+++ front/src/components/modules/static/keyword/Keyword.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Text from 'components/atoms/text/Text';
 import { KeywordsProps } from 'types/atoms';
+import {
+  Container,
+  TextRank,
+  Title,
+  WordBoard,
+  WordCount,
+} from './Keywords.style';
 
 function Keyword({ keywords }: KeywordsProps) {
-  return <div>Keyword</div>;
+  return (
+    <Container>
+      <Title>Text Rank</Title>
+      <TextRank>
+        {keywords?.map((word, index) => (
+          <WordBoard key={word}>
+            <Text size='sm'>{word}</Text>
+            <WordCount>{index + 1}</WordCount>
+          </WordBoard>
+        ))}
+      </TextRank>
+    </Container>
+  );
 }
 
 export default Keyword;

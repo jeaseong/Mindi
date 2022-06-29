aaf0bfbd50b3a0524707c7f565ce817722814e6f
12yoonj <12yoonj@gmail.com>
2022-06-29 22:06:56 +0900

    [BE] refactor: date 및 image 관련 코드 추가

--- back/src/services/ml.ts
+++ back/src/services/ml.ts
@@ -1,10 +1,9 @@
-import { StatusError } from "../utils";
+import { StatusError, imageDelete } from "../utils";
 import { Service, Inject } from "typedi";
 import { MongoDiaryModel, MongoStatModel } from "../models";
 import winston from "winston";
 import axios from "axios";
 import dayjs from "dayjs";
-import "dayjs/locale/ko";
 
 @Service()
 export default class MLService {
@@ -14,9 +13,15 @@ export default class MLService {
     @Inject("logger") private logger: winston.Logger,
   ) {}
 
-  public async postSentimentAnalysis(feeling: string, userId?: string, diaryDate?: string) {
+  public async postSentimentAnalysis(
+    feeling: string,
+    userId?: string,
+    diaryDate?: string,
+    imageFileName?: string,
+  ) {
+    // 다이어리 업데이트의 경우, 날짜 확인 X
     if (userId && diaryDate) {
-      const doc = await this.diaryModel.exists(userId, { diaryDate });
+      const doc = await this.diaryModel.exists(userId, { diaryDate: dayjs(diaryDate).toDate() });
       if (doc) {
         throw new StatusError(400, "해당 날짜의 일기가 이미 존재합니다.");
       }
@@ -26,20 +31,21 @@ export default class MLService {
       const { data } = await axios.post("/sentiment", { feeling });
       return data.result;
     } catch (error) {
+      if (imageFileName) {
+        await imageDelete(imageFileName);
+      }
       throw new StatusError(400, "감정 분석에 실패하였습니다.");
     }
   }
 
   public async postKeywordAnalysis(userId: string, date: string) {
-    const doc = await this.statModel.exists(userId, { monthly: date });
+    const doc = await this.statModel.exists(userId, { monthly: dayjs(date).toDate() });
     if (doc) {
       throw new StatusError(400, "분석 결과가 이미 존재합니다.");
     }
 
-    //TODO: 프론트에서 iso로 주면 지우기!
-    dayjs.locale("ko");
-    const from = dayjs(date).startOf("month").format();
-    const to = dayjs(date).endOf("month").format();
+    const from = dayjs(date).startOf("month").toDate();
+    const to = dayjs(date).endOf("month").toDate();
 
     const docList = await this.diaryModel.findByDate(userId, from, to);
     if (docList.length == 0) {

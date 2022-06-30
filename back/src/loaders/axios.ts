eaa046c1bdc14c85b6a05e4d9869efe817e547c8
12yoonj <12yoonj@gmail.com>
2022-06-30 12:22:00 +0900

    [BE] fix: axios 시간 제한 없앰

--- back/src/loaders/axios.ts
+++ back/src/loaders/axios.ts
@@ -3,5 +3,4 @@ import config from "../config";
 
 export default async () => {
   axios.defaults.baseURL = `${config.aiURL}/diaries`;
-  axios.defaults.timeout = 10000; // 10초 제한 //TODO: 시간 제한 어쩌지...?
 };

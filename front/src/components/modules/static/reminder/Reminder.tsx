bba6fd0245562c385960ed440e3ec1e9648ea658
jjssong <wkdthdrnt@naver.com>
2022-07-01 08:34:55 +0900

    [FE][]style: 리마인더 navigate 설정

--- front/src/components/modules/static/reminder/Reminder.tsx
+++ front/src/components/modules/static/reminder/Reminder.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import SubTitle from 'components/atoms/text/SubTitle';
 import Text from 'components/atoms/text/Text';
 import Image from 'components/atoms/image/Image';
-import { selectMaxSentiment } from 'utils/utils';
+import { selectMaxSentiment, convertUtcToKst } from 'utils/utils';
 import { SENTIMENTS } from 'utils/image';
 import {
   Container,
   ReminderDiaries,
   ReminderDiary,
   Head,
-  ImgContainer,
   DiaryPreview,
 } from './Reminder.style';
 const ORDER = ['첫 번째', '두 번째', '세 번째'];
 function Reminder({ reminder }: any) {
+  const navigate = useNavigate();
+
+  const onClickDiary = (utc: Date) => {
+    const date = convertUtcToKst(utc);
+    navigate(`/result/${date}`);
+  };
+
   return (
     <Container>
       <SubTitle size='sm'>AI가 기억하는 이달의 일기</SubTitle>
@@ -21,7 +28,10 @@ function Reminder({ reminder }: any) {
         {reminder?.map((diary: any, index: number) => {
           const maxEmotion = selectMaxSentiment(diary?.sentiment).toUpperCase();
           return (
-            <ReminderDiary key={diary._id}>
+            <ReminderDiary
+              key={diary._id}
+              onClick={() => onClickDiary(diary.diaryDate)}
+            >
               <Head>
                 <Text align='left' size='sm'>
                   {ORDER[index]} 일기
@@ -29,17 +39,17 @@ function Reminder({ reminder }: any) {
                 <Image
                   src={SENTIMENTS[maxEmotion].url}
                   alt={SENTIMENTS[maxEmotion].alt}
-                  width='20px'
+                  width='30px'
                 />
               </Head>
-              <ImgContainer>
+              {/* <ImgContainer>
                 <Image
                   src={diary.imageFilePath}
                   alt='일기 썸네일'
                   width='100%'
                 />
-              </ImgContainer>
-              <DiaryPreview></DiaryPreview>
+              </ImgContainer> */}
+              <DiaryPreview>{diary.feeling}</DiaryPreview>
             </ReminderDiary>
           );
         })}

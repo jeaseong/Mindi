a886bc34ee9d80a304ca73ed69940be11fb86029
Jaeyeon Kim <fobbb77@gmail.com>
2022-06-30 14:17:54 +0900

    [BE] feat: sign-up시 패스워드 체크

--- back/src/api/middlewares/express-validator/auth.ts
+++ back/src/api/middlewares/express-validator/auth.ts
@@ -2,7 +2,15 @@ import { body } from "express-validator";
 
 export default {
   signUpBody: [
-    body("name").notEmpty().withMessage("이름 정보는 필수입니다.").bail().isString().trim(),
+    body("name")
+      .notEmpty()
+      .withMessage("이름 정보는 필수입니다.")
+      .bail()
+      .isString()
+      .trim()
+      .isLength({ min: 2 })
+      .withMessage("이름은 2글자 이상이어야 합니다.")
+      .bail(),
     body("email")
       .notEmpty()
       .withMessage("이메일 정보는 필수입니다.")
@@ -10,7 +18,19 @@ export default {
       .isEmail()
       .bail()
       .normalizeEmail(),
-    body("password").notEmpty().withMessage("비밀번호는 필수입니다.").bail().isString(),
+    body("password")
+      .notEmpty()
+      .withMessage("비밀번호는 필수입니다.")
+      .bail()
+      .isStrongPassword({
+        minLength: 8,
+        minNumbers: 1,
+        minLowercase: 1,
+        minUppercase: 0,
+        minSymbols: 0
+      })
+      .withMessage("비밀번호를 확인해주세요.")
+      .bail()
   ],
   signInBody: [
     body("email")

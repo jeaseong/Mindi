e8caa522474bcd32b6c14284732d4b9ad791ca17
Eunyeoul Lee <yiio08@naver.com>
2022-06-30 11:42:46 +0900

    [FE][] feat: modal 클릭 시 배경 스크롤 잠금

--- front/src/components/modules/bamboo/modal/modal.tsx
+++ front/src/components/modules/bamboo/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
 import { ModalContainer, DialogBox, Backdrop } from './modal.style';
 
 interface ModalDefaultType {
@@ -9,6 +9,19 @@ function Modal({
   onClickToggleModal,
   children,
 }: PropsWithChildren<ModalDefaultType>) {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
   return (
     <>
       <ModalContainer>

e7512181018e5b5f67391bca9ab7f68fdef93c14
12yoonj <12yoonj@gmail.com>
2022-07-01 00:21:58 +0900

    [BE] test: auth, user api-test 문서 수정

--- back/tests/api-test/auth.test.ts
+++ back/tests/api-test/auth.test.ts
@@ -10,17 +10,25 @@ describe("Auth Router Test", () => {
     const response = await request(server).post("/auth/local/sign-up").send({
       email: "test@test.com",
       name: "test",
-      password: "1234",
+      password: "test1234",
     });
     expect(response.status).toEqual(200);
+    expect(response.body.result).toHaveProperty("_id", expect.any(String));
+    expect(response.body.result).toHaveProperty("email", "test@test.com");
+    expect(response.body.result).toHaveProperty("name", "test");
   });
 
   it("Sign-in test", async () => {
     const response = await request(server).post("/auth/local/sign-in").send({
       email: "test@test.com",
-      password: "1234",
+      password: "test1234",
     });
     expect(response.status).toEqual(200);
+    expect(response.body.result).toHaveProperty("_id", expect.any(String));
+    expect(response.body.result).toHaveProperty("email", "test@test.com");
+    expect(response.body.result).toHaveProperty("name", "test");
+    expect(response.body.result).toHaveProperty("token", expect.any(String));
+    expect(response.body.result).toHaveProperty("expiresIn", "1d");
   });
 });
 

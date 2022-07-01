f190cf24171a14c1a9f9bf9892c63a0e170e4a00
12yoonj <12yoonj@gmail.com>
2022-07-01 22:21:46 +0900

    [BE] fix: delete all 오류 해결 및 포스트 삭제 시, 댓글도 삭제

--- back/src/interfaces/IPostModel.ts
+++ back/src/interfaces/IPostModel.ts
@@ -4,7 +4,7 @@ import { IPost } from "./IPost";
 export interface IPostModel {
   create: (body: Partial<IPost>) => Promise<Partial<IPost>>;
   update: (filter: Partial<IPost>, fieldToUpdate: Partial<IPost>) => Promise<Partial<IPost> | null>;
-  delete: (postId: string) => Promise<void>;
+  delete: (postId: string, session: ClientSession) => Promise<void>;
   findOne: (filter: Partial<IPost>) => Promise<Partial<IPost> | null>;
   findMany: (
     filter: Partial<IPost> | null,

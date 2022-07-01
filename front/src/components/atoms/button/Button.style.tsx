64e805cd5d750b31862ad514141795a870200ed3
jjssong <wkdthdrnt@naver.com>
2022-07-01 16:07:36 +0900

    [FE][]style: 크기. 높이 조정

--- front/src/components/atoms/button/Button.style.tsx
+++ front/src/components/atoms/button/Button.style.tsx
@@ -3,9 +3,11 @@ import { BtnProps } from 'types/atoms';
 
 export const Btn = styled.button<BtnProps>`
   background-color: ${(props) => props.theme.colors.btnColor};
-  width: 100px;
+  width: 130px;
   height: 40px;
-  font-size: 0.7rem;
+  font-size: 0.8rem;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.basicBlack};
   border: none;
   cursor: pointer;
   transition: all 0.1s ease-in-out;
@@ -13,6 +15,6 @@ export const Btn = styled.button<BtnProps>`
     background-color: ${(props) => props.theme.colors.btnColorHover};
   }
   ${({ theme }) => theme.media.tablet`
-    width: 130px;
+    width: 150px;
   `}
 `;

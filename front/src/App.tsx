860b9f5068cc0f6fa4062f05849eef5a412ce0ed
jjssong <wkdthdrnt@naver.com>
2022-07-02 04:43:44 +0900

    [FE][]chore: 스낵바 위치 가운데로 변경, 유저에게 에러 메세지를 정확히 보여주기 위해

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <div className='App'>
       <RoutePage isFetching={isLoading} isLogin={isLogin} />
-      <SnackbarContainer />
+      <SnackbarContainer position='center' />
     </div>
   );
 };

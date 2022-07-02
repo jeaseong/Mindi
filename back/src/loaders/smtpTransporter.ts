17c1973e243796f189b10eed30a2fa3b3ecac994
12yoonj <12yoonj@gmail.com>
2022-07-02 12:55:24 +0900

    [BE] fix: nodmailer에 host 추가

--- back/src/loaders/smtpTransporter.ts
+++ back/src/loaders/smtpTransporter.ts
@@ -3,6 +3,7 @@ import config from "../config";
 
 export default nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
   auth: {
     user: config.nodemailerID,
     pass: config.nodemailerPW,
